39c39993388bd8f42b9011674b3202a9eb9cc4ff
customcommander <hello@spinjs.com>
2020-01-03 09:02:10 +0000

    refactor: refactor and document intersperse

--- src/utils/intersperse.js
+++ src/utils/intersperse.js
@@ -21,7 +21,27 @@
  * SOFTWARE.
  */
 
+const init = require('./init');
+
+/**
+ * Put `b` inside `a` at regular intervals.
+ *
+ * @example
+ * intersperse([1, 3, 5], [2, 4])
+ * //=> [1, 2, 3, 4, 5]
+ *
+ * @param {Array<?>} a
+ * @param {Array<?>} b
+ * @return {Array<?>}
+ */
 const intersperse = (a, b) =>
-  a.flatMap((x, i) => [x, b[i]]).slice(0, -1);
+  init
+    ( a.flatMap
+        ( (x, i) =>
+            [ x
+            , b[i]
+            ]
+        )
+    );
 
 module.exports = intersperse;
