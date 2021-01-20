88c2747f413f1ec41c2abb8c7989b014c79cd6ce
customcommander <hello@spinjs.com>
2021-01-20 21:21:05 +0000

    refactor: cosmetic changes for intersperse

--- src/utils/intersperse.js
+++ src/utils/intersperse.js
@@ -6,24 +6,14 @@
 const {init} = require('./fp');
 
 /**
- * Put `b` inside `a` at regular intervals.
+ * Put `ys` inside `xs` at regular intervals.
  *
  * @example
  * intersperse([1, 3, 5], [2, 4])
  * //=> [1, 2, 3, 4, 5]
  *
- * @param {Array<?>} a
- * @param {Array<?>} b
- * @return {Array<?>}
+ * @param {Array} xs
+ * @param {Array} ys
+ * @return {Array}
  */
-const intersperse = (a, b) =>
-  init
-    ( a.flatMap
-        ( (x, i) =>
-            [ x
-            , b[i]
-            ]
-        )
-    );
-
-module.exports = intersperse;
+module.exports = (xs, ys) => init(xs.flatMap((x, i) => [x, ys[i]]));
