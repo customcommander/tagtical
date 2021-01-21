6d8251205d630d40a72986f135a4e26079e7260e
customcommander <hello@spinjs.com>
2021-01-21 00:27:20 +0000

    refactor: deprecate tags composition and user-defined tags
    This simplifies a few things and some code has been deleted as a result.
    
    BREAKING CHANGE: users cannot define their own tags or compose tags anymore
    
    These two features have never been "advertised" anyway so there is
    very little risk that anybody ever used them. However this is
    in effect a breakin change and therefore warrants a major release.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,13 @@
  * @copyright (c) 2021 Julien Gonzalez <hello@spinjs.com>
  */
 
-const tag_function = require('./utils/tag-function');
-const use_tag = require('./utils/use-tag');
-const intersperse = require('./utils/intersperse');
-const defaults = require('./defaults');
-const hide = require('./hide');
-const list = require('./list');
-const lower = require('./lower');
-const pluralize = require('./pluralize');
-const time = require('./time');
-const trim = require('./trim');
-const upper = require('./upper');
-
-const
-  { compose
-  , join
-  } = require('./utils/fp');
-
-const tag = (...fns) =>
-  (strs, ...vals) =>
-    compose(join(''), ...fns.map(use_tag.unwrap))
-      (intersperse(strs, vals));
-
-tag.defaults = use_tag(defaults);
-tag.list = use_tag(list);
-tag.hide = use_tag(hide);
-tag.lower = use_tag(lower);
-tag.pluralize = use_tag(pluralize);
-tag.time = use_tag(time);
-tag.trim = use_tag(trim);
-tag.upper = use_tag(upper);
-tag.of = compose(use_tag, tag_function);
-
-module.exports = tag;
+module.exports = {
+  defaults: require('./defaults'),
+  hide: require('./hide'),
+  list: require('./list'),
+  lower: require('./lower'),
+  pluralize: require('./pluralize'),
+  time: require('./time'),
+  trim: require('./trim'),
+  upper: require('./upper')
+};
